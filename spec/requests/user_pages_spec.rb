0ea0f1f6eec5af994589a09ffb5f711b2b3889ac
zengfen <zengfen@xjgreat.com>
2018-11-30 10:41:26 +0800

    test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "User pages" do
   subject { page }
   describe "signup page" do
-    before { visit signup_path }
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    before { visit signup_path}
+    it { should have_content('Sign up')}
+    it { should have_title(full_title('Sign up'))}
   end
 end
